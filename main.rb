ca6a63657bb69a895ce68325cb20749e668ce460
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-18 14:43:14 +1000

    add geocoder gem, models file and indexing code

--- main.rb
+++ main.rb
@@ -3,6 +3,16 @@ require 'sinatra'
 require 'active_record'
 require_relative 'db_config'
 
+require'models/location'
+
+def index
+  if params[:search].present?
+    @locations = Location.near(params[:search], 50, :order => :distance)
+  else
+    @locations = Location.all
+  end
+end
+
 
 get '/' do
   erb :index
