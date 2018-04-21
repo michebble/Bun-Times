9d867e24f1f9d832ab4cea1005be898d629338d2
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-21 21:13:22 +1000

    create password confirmation check in js

---
+++ public/javascripts/application.js
@@ -0,0 +1,10 @@
+var check = function() {
+
+  if (document.getElementById('password').value == document.getElementById('confirm_password').value) {
+    document.getElementById('confirm_password').style.color = 'black';
+    document.getElementById('signup-btn').disabled= false;
+  } else {
+    document.getElementById('confirm_password').style.color = 'red';
+    document.getElementById('signup-btn').disabled= true;
+  }
+}
